4d930bc46cdfff873a5fc37c40483d849577619d
ESS User <User@example.com>
2022-03-01 14:17:27 -0500

    End of Day

---
+++ Challenge.js
@@ -0,0 +1,14 @@
+let square = [
+	[1, 2, 7],
+	[4, 5, 4],
+	[7, 1, 2]
+]
+
+let n = square.length
+
+for (let n = 0; n < 3; n++) {
+    for (let i = 0; n < 3; i++) {
+        let row = square[i+n]
+    }}
+
+console.log(n)
