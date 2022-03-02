38a0234a3e5b7861eafa9aa5fe529dbb41c7a429
ESS User <User@example.com>
2022-03-02 13:59:29 -0500

    Challenged failed

--- Challenge.js
+++ Challenge.js
@@ -3,12 +3,15 @@ let square = [
 	[4, 5, 4],
 	[7, 1, 2]
 ]
-
+dw
 let n = square.length
 
-for (let n = 0; n < 3; n++) {
-    for (let i = 0; n < 3; i++) {
-        let row = square[i+n]
-    }}
+function rows() {
+for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+        for (let i = 0; i < n ; i++)
+        let row1 = square.row[i]
+        console.log(row1)
+    }}}
 
-console.log(n)
+rows()
